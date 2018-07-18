8350aae5b8ff9f8a67f58bd085c96eb042788d65
pryre.dev@outlook.com <pryre.dev@outlook.com>
2018-07-18 10:49:26 +1000

    Starting spline integration

--- contrail/include/contrail/ContrailManager.h
+++ contrail/include/contrail/ContrailManager.h
@@ -17,6 +17,64 @@
 #include <mavros_msgs/PositionTarget.h>
 
 #include <eigen3/Eigen/Dense>
+#include <eigen3/unsupported/Eigen/Splines>
+
+#ifdef DO_NO_USE
+#include <Eigen/Core>
+#include <unsupported/Eigen/Splines>
+
+#include <iostream>
+
+class SplineFunction {
+public:
+  SplineFunction(Eigen::VectorXd const &x_vec,
+                 Eigen::VectorXd const &y_vec)
+    : x_min(x_vec.minCoeff()),
+      x_max(x_vec.maxCoeff()),
+      // Spline fitting here. X values are scaled down to [0, 1] for this.
+      spline_(Eigen::SplineFitting<Eigen::Spline<double, 1>>::Interpolate(
+                y_vec.transpose(),
+                 // No more than cubic spline, but accept short vectors.
+
+                std::min<int>(x_vec.rows() - 1, 3),
+                scaled_values(x_vec)))
+  { }
+
+  double operator()(double x) const {
+    // x values need to be scaled down in extraction as well.
+    return spline_(scaled_value(x))(0);
+  }
+
+private:
+  // Helpers to scale X values down to [0, 1]
+  double scaled_value(double x) const {
+    return (x - x_min) / (x_max - x_min);
+  }
+
+  Eigen::RowVectorXd scaled_values(Eigen::VectorXd const &x_vec) const {
+    return x_vec.unaryExpr([this](double x) { return scaled_value(x); }).transpose();
+  }
+
+  double x_min;
+  double x_max;
+
+  // Spline of one-dimensional "points."
+  Eigen::Spline<double, 1> spline_;
+};
+
+int main(int argc, char const* argv[])
+{
+  Eigen::VectorXd xvals(3);
+  Eigen::VectorXd yvals(xvals.rows());
+
+  xvals << 0, 15, 30;
+  yvals << 0, 12, 17;
+
+  SplineFunction s(xvals, yvals);
+
+  std::cout << s(12.34) << std::endl;
+}
+#endif
 
 class ContrailManager {
 	public:
@@ -50,6 +108,13 @@ class ContrailManager {
 		ros::Duration param_hold_duration_;
 		double param_dsp_radius_;
 		double param_dsp_yaw_;
+		/*
+		Eigen::Spline spline_px_;	//Position X
+		Eigen::Spline spline_py_;	//Position Y
+		Eigen::Spline spline_pz_;	//Position Z
+		Eigen::Spline spline_ry_;	//Yaw
+		*/
+		Eigen::Spline<double, 4> spline_;	//[X,Y,Z,y]
 
 		TrackingRef tracked_ref_;
 
